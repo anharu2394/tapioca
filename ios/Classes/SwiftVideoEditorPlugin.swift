cae8e8a1e44f5778ca0c016bae2a3e3e03aca43a
Anharu <haruan2394@gmail.com>
2020-04-27 06:18:37 +0900

    Change: Change SwiftVideoEditorPlugin

--- ios/Classes/SwiftVideoEditorPlugin.swift
+++ ios/Classes/SwiftVideoEditorPlugin.swift
@@ -9,6 +9,31 @@ public class SwiftVideoEditorPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    switch call.method {
+    case "writeVideofile":
+        let video = VideoGeneratorService()
+        guard let args = call.arguments as? [String: Any] else {
+          result(FlutterError(code: "arguments_not_found",
+                            message: "the arguments is not found.",
+                            details: nil))
+          return
+        }
+        guard let srcName = args["name"] as? String else {
+                result(FlutterError(code: "file_name_not_found",
+                                  message: "the file name is not found.",
+                                  details: nil))
+                return
+        }
+        guard let processing = args["processing"] as?  [String: [String: Any]] else {
+                result(FlutterError(code: "processing_data_not_found",
+                                  message: "the processing is not found.",
+                                  details: nil))
+                return
+        }
+        video.writeVideofile(srcPath: srcName,processing: processing,result: result)
+        result(nil)
+    default:
+        result("iOS d" + UIDevice.current.systemVersion)
+    }
   }
 }

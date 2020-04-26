bc0d904c9ed55fa84aa9061cacfab06596e35d05
Anharu <haruan2394@gmail.com>
2020-04-27 06:19:38 +0900

    Add: Create VideoGeneratorService

--- ios/Classes/VideoEditorPlugin.m
+++ ios/Classes/VideoEditorPlugin.m
@@ -1,11 +1,11 @@
 #import "VideoEditorPlugin.h"
-#if __has_include(<video_editor/video_editor-Swift.h>)
-#import <video_editor/video_editor-Swift.h>
+#if __has_include(<tapioca/tapioca-Swift.h>)
+#import <tapioca/tapioca-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "video_editor-Swift.h"
+#import "tapioca-Swift.h"
 #endif
 
 @implementation VideoEditorPlugin

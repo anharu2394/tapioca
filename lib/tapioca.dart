345f3d5aef1601faa107556bea5983a896c01e27
Anharu <haruan2394@gmail.com>
2020-04-27 06:24:27 +0900

    Change: Export files

--- lib/tapioca.dart
+++ lib/tapioca.dart
@@ -1,19 +1,3 @@
-import 'dart:async';
-import 'dart:io';
-
-import 'package:flutter/services.dart';
-import 'src/tapioca_ball.dart';
-
-class VideoEditor {
-  static const MethodChannel _channel =
-      const MethodChannel('video_editor');
-
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
-
-  static Future<File> export() async {
-
-  }
-}
+export 'package:tapioca/src/cup.dart';
+export 'package:tapioca/src/content.dart';
+export 'package:tapioca/src/tapioca_ball.dart';

8b20531fd248a57efa67b1ec7685153b5b935512
Anharu <haruan2394@gmail.com>
2020-04-28 12:43:33 +0900

    Change: Reindent

--- android/src/main/kotlin/me/anharu/video_editor/VideoEditorPlugin.kt
+++ android/src/main/kotlin/me/anharu/video_editor/VideoEditorPlugin.kt
@@ -24,101 +24,105 @@ import io.flutter.plugin.common.PluginRegistry
 
 
 /** VideoEditorPlugin */
-public class  VideoEditorPlugin: FlutterPlugin, MethodCallHandler, PluginRegistry.RequestPermissionsResultListener, ActivityAware {
-  var activity: Activity? = null
-  private var methodChannel: MethodChannel? = null
-  private val myPermissionCode = 34264
+public class VideoEditorPlugin : FlutterPlugin, MethodCallHandler, PluginRegistry.RequestPermissionsResultListener, ActivityAware {
+    var activity: Activity? = null
+    private var methodChannel: MethodChannel? = null
+    private val myPermissionCode = 34264
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    onAttachedToEngine(flutterPluginBinding.binaryMessenger)
-  }
-
-  fun onAttachedToEngine(messenger: BinaryMessenger) {
-    methodChannel = MethodChannel(messenger, "video_editor")
-    methodChannel?.setMethodCallHandler(this)
-  }
-
-
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
-  companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
-      val instance = VideoEditorPlugin()
-      instance.onAttachedToEngine(registrar.messenger())
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        onAttachedToEngine(flutterPluginBinding.binaryMessenger)
     }
-  }
 
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else if (call.method == "writeVideofile"){
-
-      var getActivity = activity ?: return
-      checkPermission(getActivity)
-
-      val fileName: String = call.argument("name")?: run {
-        result.error("file_name_not_found","the file name is not found.",null)
-        return
-      }
-      val processing: HashMap<String,HashMap<String,Any>> = call.argument("processing")?: run {
-        result.error("processing_data_not_found","the processing is not found.",null)
-        return
-      }
-      val path: String = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES).getAbsolutePath();
-      println(path)
-      val generator = VideoGeneratorService(Mp4Composer(fileName, path +  File.separator +"this.mp4"))
-      generator.writeVideofile(processing, result, getActivity)
-    } else {
-      result.notImplemented()
+    fun onAttachedToEngine(messenger: BinaryMessenger) {
+        methodChannel = MethodChannel(messenger, "video_editor")
+        methodChannel?.setMethodCallHandler(this)
     }
-  }
-  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-    activity = binding.activity
-    binding.addRequestPermissionsResultListener(this)
-  }
 
-  override fun onDetachedFromActivity() {
-    methodChannel!!.setMethodCallHandler(null)
-    methodChannel = null
-  }
 
-  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
-    onAttachedToActivity(binding)
-  }
-
-  override fun onDetachedFromActivityForConfigChanges() {
-    onDetachedFromActivity()
-  }
-
-  override fun onRequestPermissionsResult( requestCode: Int, permissions: Array<out String>?,
-                                           grantResults: IntArray?): Boolean {
-    when (requestCode) {
-      myPermissionCode -> {
-        // Only return true if handling the requestCode
-        return true
-      }
+    // This static function is optional and equivalent to onAttachedToEngine. It supports the old
+    // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
+    // plugin registration via this function while apps migrate to use the new Android APIs
+    // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
+    //
+    // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
+    // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
+    // depending on the user's project. onAttachedToEngine or registerWith must both be defined
+    // in the same class.
+    companion object {
+        @JvmStatic
+        fun registerWith(registrar: Registrar) {
+            val instance = VideoEditorPlugin()
+            instance.onAttachedToEngine(registrar.messenger())
+        }
     }
-    return false
-  }
 
-  // Invoked either from the permission result callback or permission check
-  private fun completeInitialization() {
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        if (call.method == "getPlatformVersion") {
+            result.success("Android ${android.os.Build.VERSION.RELEASE}")
+        } else if (call.method == "writeVideofile") {
 
-  }
+            var getActivity = activity ?: return
+            checkPermission(getActivity)
 
-  private fun checkPermission(activity: Activity) {
-      ActivityCompat.requestPermissions(activity,
-              arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE), myPermissionCode )
-  }
+            val srcFilePath: String = call.argument("srcFilePath") ?: run {
+                result.error("src_file_path_not_found", "the src file path is not found.", null)
+                return
+            }
+            val destFilePath: String = call.argument("destFilePath") ?: run {
+                result.error("dest_file_path_not_found", "the dest file path is not found.", null)
+                return
+            }
+            val processing: HashMap<String, HashMap<String, Any>> = call.argument("processing")
+                    ?: run {
+                        result.error("processing_data_not_found", "the processing is not found.", null)
+                        return
+                    }
+            val generator = VideoGeneratorService(Mp4Composer(srcFilePath, destFilePath))
+            generator.writeVideofile(processing, result, getActivity)
+        } else {
+            result.notImplemented()
+        }
+    }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-  }
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        activity = binding.activity
+        binding.addRequestPermissionsResultListener(this)
+    }
+
+    override fun onDetachedFromActivity() {
+        methodChannel!!.setMethodCallHandler(null)
+        methodChannel = null
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        onAttachedToActivity(binding)
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+        onDetachedFromActivity()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>?,
+                                            grantResults: IntArray?): Boolean {
+        when (requestCode) {
+            myPermissionCode -> {
+                // Only return true if handling the requestCode
+                return true
+            }
+        }
+        return false
+    }
+
+    // Invoked either from the permission result callback or permission check
+    private fun completeInitialization() {
+
+    }
+
+    private fun checkPermission(activity: Activity) {
+        ActivityCompat.requestPermissions(activity,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE), myPermissionCode)
+    }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    }
 }

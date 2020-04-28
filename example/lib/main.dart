bf8a769f124e2f8edad69480aa447bc011f657a4
Anharu <haruan2394@gmail.com>
2020-04-28 12:44:28 +0900

    Change: Update example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -6,6 +6,9 @@ import 'package:flutter/services.dart' show rootBundle;
 import 'package:tapioca/src/video_editor.dart';
 import 'package:tapioca/tapioca.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:gallery_saver/gallery_saver.dart';
+import 'package:video_player/video_player.dart';
 
 void main() => runApp(MyApp());
 
@@ -16,7 +19,9 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   String _platformVersion = 'Unknown';
+  final navigatorKey = GlobalKey<NavigatorState>();
   File _video;
+  bool isLoading = false;
 
   @override
   void initState() {
@@ -45,63 +50,131 @@ class _MyAppState extends State<MyApp> {
   }
 
   _pickVideo() async {
+
     try {
       File video = await ImagePicker.pickVideo(source: ImageSource.gallery);
       print(video.path);
       setState(() {
         _video = video;
+        isLoading = true;
       });
-    } catch(error) {
+    } catch (error) {
       print(error);
     }
-
   }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: navigatorKey,
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
         body: Center(
-          child: Column(
-            children: [
-              Text('Running on: $_platformVersion\n'),
-              RaisedButton(child: Text("pick video"),
-                color: Colors.orange,
-                textColor: Colors.white,
-                onPressed: () async {
-                  print("clicked pick!");
-                  _pickVideo();
-                },
-              ),
-              RaisedButton(child: Text("click me"),
-                color: Colors.orange,
-                textColor: Colors.white,
-                onPressed: () async {
-                  print("clicked!");
-                  final imageBitmap = (await rootBundle.load("assets/tapioca_drink.png")).buffer.asUint8List();
-                  try {
-                    final tapiocaBalls = [
-                      TapiocaBall.filter(Filters.pink),
-                      TapiocaBall.imageOverlay(imageBitmap, 300, 300),
-                      TapiocaBall.textOverlay("text",100,10,100,Color(0xffffc0cb)),
-                    ];
-                    if(_video != null) {
-                      final cup = Cup(Content(_video.path), tapiocaBalls);
-                      cup.suckUp();
-                    } else {
-                      print("video is null");
-                    }
-                  } on PlatformException {
-                    print("error!!!!");
-                  }
-                },
+            child: isLoading ? CircularProgressIndicator() : RaisedButton(
+          child: Text("Pick a video and Edit it"),
+          color: Colors.orange,
+          textColor: Colors.white,
+          onPressed: () async {
+            print("clicked!");
+            await _pickVideo();
+            var tempDir = await getTemporaryDirectory();
+            final path = '${tempDir.path}/result.mp4';
+            print(tempDir);
+            final imageBitmap =
+                (await rootBundle.load("assets/tapioca_drink.png"))
+                    .buffer
+                    .asUint8List();
+            try {
+              final tapiocaBalls = [
+                TapiocaBall.filter(Filters.pink),
+                TapiocaBall.imageOverlay(imageBitmap, 300, 300),
+                TapiocaBall.textOverlay(
+                    "text", 100, 10, 100, Color(0xffffc0cb)),
+              ];
+              if (_video != null) {
+                final cup = Cup(Content(_video.path), tapiocaBalls);
+                cup.suckUp(path).then((_) async {
+                  print("finished");
+                  GallerySaver.saveVideo(path).then((bool success) {
+                    print(success.toString());
+                  });
+                  navigatorKey.currentState.push(
+                    MaterialPageRoute(builder: (context) => VideoScreen(path)),
+                  );
+                  setState(() {
+                    isLoading = false;
+                  });
+                });
+              } else {
+                print("video is null");
+              }
+            } on PlatformException {
+              print("error!!!!");
+            }
+          },
+        )),
+      ),
+    );
+  }
+}
+
+class VideoScreen extends StatefulWidget {
+  final String path;
+
+  VideoScreen(this.path);
+
+  @override
+  _VideoAppState createState() => _VideoAppState(path);
+}
+
+class _VideoAppState extends State<VideoScreen> {
+  final String path;
+
+  _VideoAppState(this.path);
+
+  VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.file(File(path))
+      ..initialize().then((_) {
+        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+        setState(() {});
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _controller.value.initialized
+            ? AspectRatio(
+                aspectRatio: _controller.value.aspectRatio,
+                child: VideoPlayer(_controller),
               )
-            ]
-          ),
+            : Container(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _controller.value.isPlaying
+                ? _controller.pause()
+                : _controller.play();
+          });
+        },
+        child: Icon(
+          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
   }
 }

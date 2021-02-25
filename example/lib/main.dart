e3141c01b0201f60b9c6fbda0f7163d82dde4463
Jay Gajjar <gajjarjay52@gmail.com>
2021-02-25 11:33:33 +0530

    Update Example
    if video stop then replay !
    and add AppBar

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -194,9 +194,16 @@ class _VideoAppState extends State<VideoScreen> {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           setState(() {
-            _controller.value.isPlaying
-                ? _controller.pause()
-                : _controller.play();
+            if (!_controller.value.isPlaying &&
+                _controller.value.initialized &&
+                (_controller.value.duration == _controller.value.position)) {
+              _controller.initialize();
+              _controller.play();
+            } else {
+              _controller.value.isPlaying
+                  ? _controller.pause()
+                  : _controller.play();
+            }
           });
         },
         child: Icon(

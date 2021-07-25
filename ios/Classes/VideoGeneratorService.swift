42ed07a1de0d04aac8b65d4b3cc6d9c2119e0d81
Anharu <haruan2394@gmail.com>
2021-07-25 22:36:44 +0900

    format

--- ios/Classes/VideoGeneratorService.swift
+++ ios/Classes/VideoGeneratorService.swift
@@ -157,31 +157,31 @@ public class VideoGeneratorService: VideoGeneratorServiceInterface {
     }
 }
 
-   extension UIColor {
+extension UIColor {
     convenience init(hex: String, alpha: CGFloat = 1.0) {
-      let v = Int("000000" + hex, radix: 16) ?? 0
-      let r = CGFloat(v / Int(powf(256, 2)) % 256) / 255
-      let g = CGFloat(v / Int(powf(256, 1)) % 256) / 255
-      let b = CGFloat(v / Int(powf(256, 0)) % 256) / 255
-      self.init(red: r, green: g, blue: b, alpha: min(max(alpha, 0), 1))
+        let v = Int("000000" + hex, radix: 16) ?? 0
+        let r = CGFloat(v / Int(powf(256, 2)) % 256) / 255
+        let g = CGFloat(v / Int(powf(256, 1)) % 256) / 255
+        let b = CGFloat(v / Int(powf(256, 0)) % 256) / 255
+        self.init(red: r, green: g, blue: b, alpha: min(max(alpha, 0), 1))
     }
-  }
+}
 
-  struct Filter {
+struct Filter {
     let type: String
+    
+}
 
-  }
-
-  struct ImageOverlay {
+struct ImageOverlay {
     let bitmap: Data
     let x: NSNumber
     let y: NSNumber
-  }
+}
 
-  struct TextOverlay {
+struct TextOverlay {
     let text: String
     let x: NSNumber
     let y: NSNumber
     let size: NSNumber
     let color: String
-  }
+}
